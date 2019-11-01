d5ecbed764d2e4d71a59b8791f28e53f0b683fbb
msearles25 <skaman4459@gmail.com>
2019-11-01 11:36:55 -0400

    tabs done, maybe?

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,21 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        tab(response.data.topics);
+    })
+    .catch(err => {
+        console.log('You messed up the page', error);
+    })
+
+    function tab(array) {
+        const topics = document.querySelector('.topics');
+
+        array.forEach(tabName => {
+            topics.appendChild(document.createElement('div')).textContent = tabName;
+        });
+
+        return tab;
+    }
